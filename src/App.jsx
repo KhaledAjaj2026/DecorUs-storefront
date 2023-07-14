e25f352380ef07a9ba49b2e9efe5da27c5035529
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-14 12:47:01 -0700

    feat: add color palette variables and font families.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <nav id="navbar">
+        <p>Home</p>
+        <p>Shop</p>
+        <p>About</p>
+        <p>Contact</p>
+      </nav>
       <div>
-        he
+        <h2 className="label">Suade Reading Chair</h2>
+        <p className="description">
+          Grab your favorite book and a warm cup of coffee as you lounge on this
+          wonderful option of a reading chair.
+        </p>
+        <p className="price">129.99</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
