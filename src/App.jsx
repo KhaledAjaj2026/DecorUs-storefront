d6d6efbcb21e9b8ca7c4814dcaeb8d371db88f7a
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-14 17:33:55 -0700

    feat: add navbar component and styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
-      <nav id="navbar">
-        <p>Home</p>
-        <p>Shop</p>
-        <p>About</p>
-        <p>Contact</p>
-      </nav>
+      <Navbar />
       <div>
-        <h2 className="label">Suade Reading Chair</h2>
-        <p className="description">
+        <h2 className="font-label">Suade Reading Chair</h2>
+        <p className="font-description">
           Grab your favorite book and a warm cup of coffee as you lounge on this
           wonderful option of a reading chair.
         </p>
-        <p className="price">129.99</p>
+        <p className="font-price">129.99</p>
       </div>
     </>
   );
