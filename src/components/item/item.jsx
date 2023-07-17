7eaa77416877a51806d7b4c8d78186a3ed23ab6a
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-16 17:47:15 -0700

    adding images

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -3,7 +3,10 @@ import "./item.css";
 export default function Item() {
   return (
     <>
-      <div id="component-item">item component</div>
+      <div id="component-item">
+        <img src="" alt="" />
+        <p className="item-name"></p>
+      </div>
     </>
   );
 }
