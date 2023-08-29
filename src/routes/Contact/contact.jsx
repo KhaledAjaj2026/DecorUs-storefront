59d5c088a40a88fb4148e963788d3eafd00b37e3
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-29 16:40:06 -0700

    feat: build JSX sections for Contact page; correct link to Shop in NavBar.

--- src/routes/Contact/contact.jsx
+++ src/routes/Contact/contact.jsx
@@ -6,11 +6,27 @@ export default function Contact() {
 		<>
 			<NavBar />
 			<div id="route_contact">
-				<h1>Thoughts? Inquiries?</h1>
-				<p>
-					Reach out to us and let us know of anything you need, we are always
-					happy to help.
-				</p>
+				<h1>Get In Touch</h1>
+				<p>Let us know of anything you need, we are always happy to help.</p>
+				<form action="">
+					<label htmlFor="customer-name">Name: </label>
+					<input
+						type="text"
+						name="customer-name"
+						height={200}
+						id="customer-name"
+					/>
+					<label htmlFor="customer-email">Email: </label>
+					<input type="email" name="customer-email" id="customer-email" />
+					<label htmlFor="customer-message">Message: </label>
+					<textarea
+						name="customer-message"
+						id="customer-message"
+						cols="30"
+						rows="8"
+					></textarea>
+					<button type="submit">Submit</button>
+				</form>
 			</div>
 		</>
 	);
