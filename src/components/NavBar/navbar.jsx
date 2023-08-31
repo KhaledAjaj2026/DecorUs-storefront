3e4880caefd30f793c62b25a3eb8b0ba3a7c9913
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-30 17:34:48 -0700

    feat: change Link styling to 'a'.
    I am an idioooooooot, I could have just styled the 'a' tags since R-Router just renders 'a' tags from their Link components; remove superfluous code for styling Link tags and style 'a' tags instead.

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -3,30 +3,13 @@ import logo from '../../assets/logo-no-background.svg';
 import './navbar.css';
 
 export default function NavBar() {
-	// Necessary for stlying ReactRouter Link components, cannot use conventional CSS methods.
-	const linkStyle = {
-		textDecoration: 'none',
-		color: 'black',
-		margin: 5,
-		fontSize: '1.5rem',
-		fontFamily: 'Libre Franklin',
-	};
-
 	return (
 		<nav id="component_navbar">
 			<div className="navbar_links">
-				<Link to="/" style={linkStyle}>
-					Home
-				</Link>
-				<Link to="/shop" style={linkStyle}>
-					Shop
-				</Link>
-				<Link to="/about" style={linkStyle}>
-					About
-				</Link>
-				<Link to="/contact" style={linkStyle}>
-					Contact
-				</Link>
+				<Link to="/">Home</Link>
+				<Link to="/shop">Shop</Link>
+				<Link to="/about">About</Link>
+				<Link to="/contact">Contact</Link>
 			</div>
 			<Link to="/" className="navbar_logo">
 				<img src={logo} width={120} alt="DecorUs logo" />
