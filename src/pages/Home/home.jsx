5af7239a92716bd6ae87924e8caa96a9ee69880e
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-09-04 16:48:07 -0700

    fix: attempting to fix Netlify build error - trial #14

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import { Link, ScrollRestoration } from 'react-router-dom';
-import Item from '/src/components/Item/item.jsx';
-import NavBar from '/src/components/NavBar/navbar.jsx';
+import Item from '/src/components/Item/item';
+import NavBar from '/src/components/NavBar/navbar';
 import item1 from '/armchair-2-min.jpg';
 import item2 from '/armchair-3-min.jpeg';
 import item3 from '/armchair-4-min.jpeg';
