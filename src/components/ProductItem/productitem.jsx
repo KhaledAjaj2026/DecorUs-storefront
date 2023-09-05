8e6d79c50b9b6bea982e3c8884682e33586cbc13
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-09-05 14:27:53 -0700

    feat: remove div surrounding ProductItem elements to fix flexbox.

--- src/components/ProductItem/productitem.jsx
+++ src/components/ProductItem/productitem.jsx
@@ -6,11 +6,9 @@ export default function Item() {
 	return (
 		<Link to="/product-details">
 			<div id="component_item">
-				<div className="item-details">
-					<img src={furnitureData[0].location} className="item-img" alt="" />
-					<p className="item-name">{furnitureData[0].name}</p>
-					<p className="item-price">{furnitureData[0].price}</p>
-				</div>
+				<img src={furnitureData[0].location} className="item-img" alt="" />
+				<p className="item-name">{furnitureData[0].name}</p>
+				<p className="item-price">{furnitureData[0].price}</p>
 			</div>
 		</Link>
 	);
