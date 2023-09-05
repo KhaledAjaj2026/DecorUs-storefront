dd31ba3a42cab1044d4a2ef6e1f6a3b8dc392eb4
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-09-05 16:16:04 -0700

    feat: delete incompatible furniture item from JSON, update references across site.

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -23,16 +23,19 @@ export default function Root() {
 						<ProductItem
 							name={furnitureData[17].name}
 							location={furnitureData[17].location}
+							description={furnitureData[17].description}
 							price={furnitureData[17].price}
 						/>
 						<ProductItem
 							name={furnitureData[7].name}
 							location={furnitureData[7].location}
+							description={furnitureData[7].description}
 							price={furnitureData[7].price}
 						/>
 						<ProductItem
 							name={furnitureData[2].name}
 							location={furnitureData[2].location}
+							description={furnitureData[2].description}
 							price={furnitureData[2].price}
 						/>
 					</div>
@@ -48,7 +51,7 @@ export default function Root() {
 							alt=""
 						/>
 						<img
-							src={furnitureData[12].location}
+							src={furnitureData[11].location}
 							width={350}
 							className="home_discover_image-right"
 							alt=""
@@ -60,7 +63,7 @@ export default function Root() {
 						<h2>Carefully Handcrafted Masterpieces</h2>
 						<p>Our stock is entirely unique, made by our in-house masters</p>
 					</div>
-					<img src={furnitureData[9].location} width={500} alt="" />
+					<img src={furnitureData[15].location} width={500} alt="" />
 				</section>
 				<section className="home_reviews">
 					<h2>{'"Best in the Industry, Hands Down"'}</h2>
