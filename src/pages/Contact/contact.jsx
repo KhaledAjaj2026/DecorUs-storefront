c1f13b5fdf7d9136d3a94eea8d34a40b7fe84c08
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-10-12 13:32:45 -0700

    feat: implement state and functions for contact form.
    Utilize useState hook to make user-input data stateful. Create handler functions for handling fetch, resetting fields, and returning status of fetch.

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,10 +1,73 @@
-import { Link, ScrollRestoration } from 'react-router-dom';
+import { useState } from 'react';
+import { ScrollRestoration } from 'react-router-dom';
 import NavBar from '/src/components/NavBar/navbar';
 import Footer from '/src/components/Footer/footer';
 import 'aos/dist/aos.css';
 import './contact.css';
 
 export default function Contact() {
+	// Data retrieved from user input from contact form.
+	const [contactData, setContactData] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
+	// Handler for inputting data to state variable.
+	const handleContactDataInput = (event) => {
+		setContactData((prevContactData) => {
+			return {
+				...prevContactData,
+				[event.target.name]: event.target.value,
+			};
+		});
+		if (result.length > 0) setResult('');
+	};
+	// Result of message being sent or not.
+	const [result, setResult] = useState('');
+	// Status while message is being sent.
+	const [status, setStatus] = useState('Submit');
+	// Reset contact form to empty once input is sumbitted.
+	const resetContactData = () => {
+		setContactData({ name: '', email: '', message: '' });
+	};
+	// Handles user-input submit.
+	const handleSubmit = async (e) => {
+		setResult('');
+		e.preventDefault();
+		setStatus('Sending...');
+
+		const { name, email, message } = e.target.elements;
+
+		let details = {
+			name: name.value,
+			email: email.value,
+			message: message.value,
+		};
+
+		try {
+			let response = await fetch('http://localhost:5000/send', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8',
+				},
+				body: JSON.stringify(details),
+			});
+			setStatus('Submit');
+			let submitResult = await response.json();
+			console.log(submitResult);
+
+			if (submitResult.status === 'success') {
+				setResult('Message Sent!');
+				resetContactData();
+			} else if (submitResult.status === 'fail') {
+				alert('Message failed to send.');
+			}
+		} catch (error) {
+			console.error(error);
+			setStatus('Submit');
+			setResult('Issues with submitting message.');
+		}
+	};
 	return (
 		<>
 			<NavBar />
@@ -13,26 +76,43 @@ export default function Contact() {
 					Get In Touch
 				</h1>
 				<p>Let us know of anything you need, we are always happy to help.</p>
-				<form>
+				<form id="contact-form" method="POST" onSubmit={handleSubmit}>
 					<label htmlFor="customer-name">Name: </label>
 					<input
 						type="text"
-						name="customer-name"
+						autoComplete="name"
+						name="name"
 						height={200}
+						required={true}
 						id="customer-name"
+						value={contactData.name}
+						onChange={handleContactDataInput}
 					/>
 					<label htmlFor="customer-email">Email: </label>
-					<input type="email" name="customer-email" id="customer-email" />
+					<input
+						type="email"
+						autoComplete="email"
+						name="email"
+						required={true}
+						id="customer-email"
+						value={contactData.email}
+						onChange={handleContactDataInput}
+					/>
 					<label htmlFor="customer-message">Message: </label>
 					<textarea
-						name="customer-message"
+						name="message"
 						id="customer-message"
+						maxLength={350}
+						placeholder="Maximum 350 characters."
 						cols="30"
 						rows="8"
+						required={true}
+						value={contactData.message}
+						onChange={handleContactDataInput}
 					></textarea>
-					<Link to="/">
-						<button className="animating-button">Submit</button>
-					</Link>
+					<button type="submit" className="animating-button">
+						{status}
+					</button>
 				</form>
 			</div>
 			<Footer />
