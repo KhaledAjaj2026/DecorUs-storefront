6588707054d743392eef390b9944ad6834d18e76
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-10-16 12:38:56 -0700

    restrucure files and redefine references.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ErrorPage from './pages/Error/error-page';
-import Home from './pages/Home/home';
-import Shop from './pages/Shop/shop';
-import ProductDetails from './pages/ProductDetails/product-details';
-import About from './pages/About/about';
-import Contact from './pages/Contact/contact';
-import Cart from './pages/Cart/cart';
+import ErrorPage from './client/pages/Error/error-page';
+import Home from './client/pages/Home/home';
+import Shop from './client/pages/Shop/shop';
+import ProductDetails from './client/pages/ProductDetails/product-details';
+import About from './client/pages/About/about';
+import Contact from './client/pages/Contact/contact';
+import Cart from './client/pages/Cart/cart';
 import './index.css';
 
 const router = createBrowserRouter([
